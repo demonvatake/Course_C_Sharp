cf0c132447680823b35653e26e667aa9009f0e71
Sergey <demonvatake@gmail.com>
2022-07-12 12:42:48 +0300

    2_2numbersMax_Min

---
+++ 1_Lesson/HW/2_2numbersMax_Min/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number b: ");
+int b = int.Parse(Console.ReadLine());
+int max;
+int min;
+
+if (a < b)
+{
+    Console.Write("max=");
+    Console.WriteLine(max = b);
+    Console.Write("min=");
+    Console.WriteLine(min = a);
+}
+else
+{
+    Console.Write("max=");
+    Console.WriteLine(max = a);
+     Console.Write("min=");
+    Console.WriteLine(min = b);
+}
