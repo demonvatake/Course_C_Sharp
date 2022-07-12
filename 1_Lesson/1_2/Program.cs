332a3b22ffe2e3c682915d7aad51a0e10d7da90e
Sergey <demonvatake@gmail.com>
2022-07-12 11:29:13 +0300

    Task 1_2

---
+++ 1_Lesson/1_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a * a == b)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("No");
+}
