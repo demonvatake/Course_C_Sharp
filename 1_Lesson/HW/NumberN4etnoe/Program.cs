66db199e9ef393d98bd6ddba0de524c6aaacf916
Sergey <demonvatake@gmail.com>
2022-07-12 14:25:23 +0300

    N 4etnoe

---
+++ 1_Lesson/HW/NumberN4etnoe/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+int i = 2;
+while (i<=a)
+{
+    Console.Write("Наши числа = ");
+    Console.WriteLine(i);
+    i=i+2;
+}
