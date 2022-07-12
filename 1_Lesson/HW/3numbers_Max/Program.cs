f5b1584e564df5a94b69cfb4b5ae83337cdd0ab2
Sergey <demonvatake@gmail.com>
2022-07-12 13:22:44 +0300

    3_3numbersMax

---
+++ 1_Lesson/HW/3numbers_Max/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number b: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number c: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (b > max)max=b;
+if (c > max) max=c;
+Console.Write ("max=");
+Console.WriteLine (max);
+
+
