b6e2a31c5e8316b35215334d2249898fcab3b4e3
Sergey <demonvatake@gmail.com>
2022-07-12 14:13:39 +0300

    HW number4etnoe

--- 1_Lesson/HW/3numbers_Max/Program.cs
+++ 1_Lesson/HW/3numbers_Max/Program.cs
@@ -11,7 +11,7 @@ int max = a;
 
 if (b > max)max=b;
 if (c > max) max=c;
-Console.Write ("max=");
+Console.Write ("max = ");
 Console.WriteLine (max);
 
 
---
+++ 1_Lesson/HW/Number4etnoe/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2==0)
+{
+    Console.Write("YES оно четное = наше число ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("No оно нечетное = наше число ");
+}
