daaf49abb5fcd9b2dc450f94b5afe36540bebc9f
Sergey <demonvatake@gmail.com>
2022-07-15 17:04:56 +0300

    ArrayLibrary

--- 2_Lesson/4_ArrayLibrary/Program.cs
+++ 2_Lesson/4_ArrayLibrary/Program.cs
@@ -21,7 +21,30 @@ void PrintArray(int[] col)
     }
 }
 
+int indexof(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+        
+    }
+    return position;
+}
+
 int[] array = new int[10];
 
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
+
+int pos = indexof(array, 4);
+Console.WriteLine(pos);
