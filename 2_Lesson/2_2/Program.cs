7b158091e0c6ebc7f51dc331cb1b2f35914914b4
Sergey <demonvatake@gmail.com>
2022-07-16 14:43:35 +0300

    2_2

---
+++ 2_Lesson/2_2/Program.cs
@@ -0,0 +1,13 @@
+﻿int MaxDigit(int Number)
+{
+    if (Number / 10 > Number % 10)
+        return Number / 10;
+    else 
+        return Number % 10;
+}
+int a = new Random().Next(10,100);
+
+Console.WriteLine(a);
+Console.WriteLine(MaxDigit(a));
+
+
