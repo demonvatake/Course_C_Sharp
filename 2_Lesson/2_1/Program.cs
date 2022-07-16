ef7e5fe7c7681227fe7a736a29c404c7ba357898
Sergey <demonvatake@gmail.com>
2022-07-16 14:34:38 +0300

    2_1

---
+++ 2_Lesson/2_1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Write a number: ");
+
+int num = int.Parse(Console.ReadLine());
+int TakeNum(int num)
+{
+    return num % 10;
+}
+
+Console.Write("последняя цифра этого числа: ");
+Console.WriteLine(TakeNum(num));
