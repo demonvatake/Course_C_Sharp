671374cb2ab56f97bcbfc50ea8f8c1b7f06cfd25
Sergey <demonvatake@gmail.com>
2022-07-17 13:42:45 +0300

    доработал задачи 1_1 . 1_2

--- 2_Lesson/HW/1_1/Program.cs
+++ 2_Lesson/HW/1_1/Program.cs
@@ -1,5 +1,14 @@
-﻿Console.WriteLine("Write a number: ");
+﻿//Console.WriteLine("Write a number: ");
+
+//int a = int.Parse(Console.ReadLine());
+
+//Console.WriteLine((a / 10) % 10);
+Console.WriteLine("Write a number: ");
 
 int a = int.Parse(Console.ReadLine());
+int Num(int a)
+{
+    return a / 10 % 10;
+}
 
-Console.WriteLine((a / 10) % 10);
+Console.WriteLine(Num(a));
--- 2_Lesson/HW/1_2/Program.cs
+++ 2_Lesson/HW/1_2/Program.cs
@@ -1,7 +1,14 @@
-﻿int a = new Random().Next(100,999);
-int b = ((a / 10 / 10) % 10);
-int c = (a % 10);
+﻿//int a = new Random().Next(100,999);
+//int b = ((a / 10 / 10) % 10);
+//int c = (a % 10);
 
-Console.WriteLine(a);
-Console.Write(b);
-Console.Write(c);
+//Console.WriteLine(a);
+//Console.Write(b);
+//Console.Write(c);
+void NumRandom()
+{
+    int a = new Random().Next(100,1000);
+    Console.WriteLine($"{a} > {a / 100}{a % 10}");
+}
+
+NumRandom();
