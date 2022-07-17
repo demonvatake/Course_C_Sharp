da210fe1b782c451c8897a994305a3bd1e7fc496
Sergey <demonvatake@gmail.com>
2022-07-17 14:12:18 +0300

    доработали 1_4

--- 2_Lesson/HW/1_4/Program.cs
+++ 2_Lesson/HW/1_4/Program.cs
@@ -2,10 +2,11 @@
 {
     string text = "no";
 
-    if (a == 7 && a == 6) text = "yes";
+    if (a == 7 || a == 6) text = "yes";
     Console.WriteLine($"{a} > {text}");
 }
 
 Weekend(6);
 Weekend(7);
+Weekend(1);
 // ....
