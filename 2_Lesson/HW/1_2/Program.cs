75bcf7fdd3d5c5739d1d912942c898de64d9485c
Sergey <demonvatake@gmail.com>
2022-07-17 13:29:58 +0300

    1_3Hw

---
+++ 2_Lesson/HW/1_2/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = new Random().Next(100,999);
+int b = ((a / 10 / 10) % 10);
+int c = (a % 10);
+
+Console.WriteLine(a);
+Console.Write(b);
+Console.Write(c);
---
+++ 2_Lesson/HW/1_3/Program.cs
@@ -0,0 +1,14 @@
+﻿void Number (int a)
+{
+    Console.Write($"{a} > ");
+    if (a < 99)
+    {
+        Console.WriteLine("Нету третьей цифры");
+        return;
+    }
+    
+    while (a > 999) a = a / 10;
+    Console.WriteLine(a % 10);
+}
+
+Number(int.Parse(Console.ReadLine()));
