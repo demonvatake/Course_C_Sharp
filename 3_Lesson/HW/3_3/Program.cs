8afb396a068c2c54612cdf26a5004ef25748dfc1
Sergey <demonvatake@gmail.com>
2022-07-21 22:57:42 +0300

    3_3HW

---
+++ 3_Lesson/HW/3_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Enter N: ");
+int N=int.Parse(Console.ReadLine());
+int CubeTable(int a)
+{
+    int i=1;
+    for(i=1;i<=a;i++)
+        Console.Write($"{Math.Pow(i,3)} ");
+    return(0);
+}
+CubeTable(N);
