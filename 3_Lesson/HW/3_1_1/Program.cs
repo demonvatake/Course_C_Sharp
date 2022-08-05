3656681a1dfe38e891770ad9db5b32580449449c
Sergey <demonvatake@gmail.com>
2022-08-05 09:09:42 +0300

    3_1_1

--- 3_Lesson/HW/3_1_1/Program.cs
+++ 3_Lesson/HW/3_1_1/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿Console.Write("Enter the 5-digit number: ");
+int a = int.Parse(Console.ReadLine());
+
+void Palindrome(int n)
+{
+    int i = 0;
+    string num = n.ToString();
+    int size = num.Length - 1;
+    string flag = "yes";
+
+    while (i < size / 2)
+    {
+        if (num[i] != num[size - 1])
+        {
+            flag = "no";
+            break;
+        }
+        i += 1;
+    }
+    Console.WriteLine($"{n} -> {flag}");
+}
+
+Palindrome(a);
+Console.WriteLine();
