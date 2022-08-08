ce9fb377dfc44bd525e23d7812fffcc89ff583e1
Sergey <demonvatake@gmail.com>
2022-08-08 17:54:53 +0300

    4_3

---
+++ 4_lesson/HW/4_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая задаёт массив из
+// 8 элементов случайными числами и выводит их на экран.
+// Оформите заполнение массива и вывод в виде функции.
+
+int[] RandomArray(int num)
+{
+    int[] array1 = new int[num];
+    for(int i=0; i < num; i++)
+        array1[i]=new Random().Next();
+    return array1;
+}
+
+void ArrayOutput(int[] array2)
+{
+    for(int i=0; i < array2.Length; i++)
+        Console.Write($"{array2[i]} ");
+}
+ArrayOutput(RandomArray(8));
