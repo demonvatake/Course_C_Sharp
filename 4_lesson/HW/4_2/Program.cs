65df4f8326c992bbae01840a90c512ac66f56109
Sergey <demonvatake@gmail.com>
2022-08-08 17:53:39 +0300

    4_2

---
+++ 4_lesson/HW/4_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+
+int DigitSum(int num)
+{
+    int Sum = 0;
+    for(int i=1; num > 0; i++)
+    {
+        Sum += num%10;
+        num /= 10;
+    }
+    return Sum;
+}
+Console.WriteLine(DigitSum(452));
