e29a7a4b32a7f13ef6baee241b0ae472810b0d5b
Sergey <demonvatake@gmail.com>
2022-08-10 14:25:09 +0300

    5_lesson_HW

---
+++ 5_lesson/HW/5_3_HW/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу
+// между максимальным и минимальным элементов массива.
+double[] RandomArray()
+{
+    Console.WriteLine("Input number of elements");
+    int num = int.Parse(Console.ReadLine());
+    Console.WriteLine("Input elemet's min");
+    int min = int.Parse(Console.ReadLine());
+    Console.WriteLine("Input elemet's max");
+    int max = int.Parse(Console.ReadLine());
+    double[] array = new double[num];
+    for(int i=0; i < num; i++)
+    {
+        array[i]=new Random().NextDouble()+ new Random().Next(min, max);
+        Console.Write($"{array[i]} ");
+    }
+    return(array);
+}
+
+double ArrayMinMaxDelta(double[] array1)
+{
+    double Min = array1[0];
+    double Max = array1[0];
+    for(int i=1; i < array1.Length; i++)
+    {
+        if(array1[i]<Min)
+            Min = array1[i];
+        if(array1[i]>Max)
+            Max = array1[i];    
+    }
+    Console.WriteLine();
+    Console.WriteLine($"{Math.Round(Max-Min, 4)} ");
+   
+    return(Max-Min);
+}
+
+ArrayMinMaxDelta(RandomArray());
