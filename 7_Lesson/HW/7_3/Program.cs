53ae8ad6687e6395e8c62b217723a1bd59b04d59
Sergey <demonvatake@gmail.com>
2022-08-18 23:10:05 +0300

    7_3

--- 7_Lesson/HW/7_3/Program.cs
+++ 7_Lesson/HW/7_3/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+
+void Print2xArray(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for(int i=0; i<row_size; i++)
+    {
+        for(int j=0; j<column_size; j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] Fill2xArray(int row, int column, int min, int max)
+{
+    int[,] arr = new int[row,column];
+    
+    for(int i=0; i<row; i++)
+    {
+        for(int j=0; j<column; j++)
+        {
+            arr[i,j]=new Random().Next(min,max+1);
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    return(arr);
+}
+
+double[] Count2xArray(int[,] arr)
+{
+    double[] Sum1 = new double[arr.GetLength(0)];
+    
+    for(int i=0; i<arr.GetLength(0); i++)
+    {
+        double Sum=0;
+        for(int j=0; j<arr.GetLength(1); j++)
+        {
+            Sum+=arr[i,j];
+        }
+        Sum1[i]=Sum/arr.GetLength(1);
+        Console.Write($"{Sum1[i]} ");
+    }
+    Console.WriteLine();
+    return(Sum1);
+}
+
+
+
+int[,] arr1 = Fill2xArray(4,4,-10,10);
+Print2xArray(arr1);
+Count2xArray(arr1);
