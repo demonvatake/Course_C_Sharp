b20ae5732d5222fc26b4b03ac5503f8a95d5219a
Sergey <demonvatake@gmail.com>
2022-08-26 06:55:30 +0300

    HW9

---
+++ 9_lesson/HW/1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значения M и N. Напишите программу,
+// которая выведет все чётные натуральные числа
+// в промежутке от M до N.
+
+void Num2(int min, int max)
+{
+    max-=max%2;
+    if (max<min) return;
+    Num2(min,max-2);
+    Console.Write($"{max}, ");
+}
+
+Num2(3,9);
